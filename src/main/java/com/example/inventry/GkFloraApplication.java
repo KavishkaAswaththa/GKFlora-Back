60a76d071b099258294be8c6965b97af2bcdec36
KavishkaAswaththa <157552673+KavishkaAswaththa@users.noreply.github.com>
2024-11-25 22:22:04 +0530

    Complete

--- src/main/java/com/example/inventry/GkFloraApplication.java
+++ src/main/java/com/example/inventry/GkFloraApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GkFloraApplication {
+public class
+GkFloraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GkFloraApplication.class, args);
