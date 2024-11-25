f73a5894507cd4d9631990a5c6b1cb817d302072
KavishkaAswaththa <157552673+KavishkaAswaththa@users.noreply.github.com>
2024-11-25 18:57:23 +0530

    new

--- src/main/java/com/example/inventry/repo/DeliveryRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.inventry.repo;
-import com.example.inventry.entity.Delivery;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DeliveryRepo extends CrudRepository<Delivery,String> {
-}
